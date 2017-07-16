e349ae7c65c3980a2e4deb1f5ad7c9a92a8026b4
danielindie95 <danielindie95@gmail.com>
2017-07-16 22:45:45 +0300

    added base usage - cmd application

--- SeleniumAutomationGenerator/Program.cs
+++ SeleniumAutomationGenerator/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace SeleniumAutomationGenerator
+﻿using System;
+
+namespace SeleniumAutomationGenerator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string projectName = args[0];
+            string webAppBaseDirectory = args.Length > 1 ? args[1] : Environment.CurrentDirectory;
+            ProjectGenerator generator = new ProjectGenerator();
+            generator.GenerateProject(webAppBaseDirectory, projectName);
         }
     }
 }
--- SeleniumAutomationGenerator/ProjectGenerator.cs
+++ SeleniumAutomationGenerator/ProjectGenerator.cs
@@ -1,20 +1,25 @@
-﻿namespace SeleniumAutomationGenerator
+﻿using System.Collections.Generic;
+using System.IO;
+using Core.Models;
+
+namespace SeleniumAutomationGenerator
 {
     public class ProjectGenerator
     {
-        public void GenerateProject(string webAppBaseDirectory, string projectName, string distDirectory = null)
+        public void GenerateProject(string webAppBaseDirectory, string projectName, string solutionName = null,
+            string distDirectory = null)
         {
             CreateSolution(projectName);
-            GenerateProject(webAppBaseDirectory, projectName, projectName);
-        }
-        public void GenerateProject(string webAppBaseDirectory, string projectName, string solutionName, string distDirectory = null)
-        {
-
+            WebFolderToCsFilesConverter converter = new WebFolderToCsFilesConverter(ComponentsFactory.Instance , new HtmlFinder());
+            List<ComponentGeneratorOutput> results = converter.GenerateClasses(webAppBaseDirectory);
+            foreach (var result in results)
+            {
+                   File.WriteAllText(result.CsFileName , result.Body); 
+            }
         }
 
         private void CreateSolution(string name)
         {
-
         }
     }
 }
